1ef7f69eebe1670c85ff319bbbdaa2026447a786
shonyjade <shonyjade@mail.ru>
2015-03-26 20:19:22 +0300

    Создана БД

--- schedule/migrations/0001_initial.py
+++ schedule/migrations/0001_initial.py
@@ -11,14 +11,56 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Administration',
+            name='Cabinet',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('login', models.CharField(max_length=15)),
-                ('password', models.CharField(max_length=20)),
+                ('cabinet_number', models.CharField(max_length=10)),
             ],
             options={
-                'db_table': 'admin',
+                'db_table': 'cabinet',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='School_class',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('class_name', models.CharField(max_length=10)),
+                ('class_max_load', models.IntegerField()),
+            ],
+            options={
+                'db_table': 'school_class',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Subject',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('subject_name', models.CharField(max_length=30)),
+                ('subject_max_load', models.IntegerField()),
+                ('sclass', models.ForeignKey(to='schedule.School_class')),
+                ('subject_cabinet', models.ForeignKey(to='schedule.Cabinet')),
+            ],
+            options={
+                'db_table': 'subject',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Teacher',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('first_name', models.CharField(max_length=50)),
+                ('last_name', models.CharField(max_length=50)),
+                ('middle_name', models.CharField(max_length=50)),
+                ('teacher_max_load', models.IntegerField()),
+                ('class_management', models.ForeignKey(to='schedule.School_class')),
+                ('teacher_cabinet', models.ForeignKey(to='schedule.Cabinet')),
+                ('teacher_subject', models.ForeignKey(to='schedule.Subject')),
+            ],
+            options={
+                'db_table': 'teacher',
             },
             bases=(models.Model,),
         ),
--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 
 # class Administration(models.Model):
@@ -7,28 +9,37 @@ from django.db import models
 #     password = models.CharField(max_length=20)
 
 
-class school_class(models.Model):
-    classname = models.CharField(max_length=4)
-   # class_teacher =
+class School_class(models.Model):
+    class Meta():
+        db_table = 'school_class'
 
-# Класс
-#     номер (5а)
-#     класс рук
-#     кабинет
-#     нагрузка?
-#
-# Учитель
-#     ФИО
-#     Предмет (1уч - мн предметов)
-#     Кабинет
-#     Часы(нагрузка)
-#
-# Предмет
-#     название
-#     сложность
-#
-# Кабинет
-#     номер
-#     учитель
-#
+    class_name = models.CharField(max_length=10) # 11а и хватит (юникод?)
+    class_max_load = models.IntegerField()
 
+class Cabinet(models.Model):
+    class Meta():
+        db_table = 'cabinet'
+
+    cabinet_number = models.CharField(max_length=10)  # char - а вдруг там буквы?
+
+
+class Subject(models.Model):
+    class Meta():
+        db_table = 'subject'
+
+    subject_name = models.CharField(max_length=30)
+    sclass = models.ForeignKey('School_class')
+    subject_cabinet = models.ForeignKey('Cabinet')
+    subject_max_load = models.IntegerField()            # максимальная нагрузка на конкретный класс
+
+class Teacher(models.Model):
+    class Meta():
+        db_table = 'teacher'
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50)
+    teacher_subject = models.ForeignKey('Subject')
+    class_management = models.ForeignKey('School_class')
+    teacher_cabinet = models.ForeignKey('Cabinet')
+    teacher_max_load = models.IntegerField()                # максимальная нагрузка по указанному предмету
